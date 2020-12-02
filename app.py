d66569390f1a29416218055b6f1f369b7cbd4e0a
mark.j0hnst0n <friendorfaux6@gmail.com>
2020-12-02 12:55:30 +0000

    incorrect lower function removed

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def add_user():
 @app.route('/profile/<user>', methods=["GET", "POST"])
 def profile(user):
 
-    username = user_collection.find_one({"username": user}).lower
+    username = user_collection.find_one({"username": user})
     my_recipes = list(recipe_collection.find({"user": user})
                       .sort("datetime", -1))
     recipe_count = len(my_recipes)
