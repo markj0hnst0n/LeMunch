2ff3011009f2c7621d149ab5a5730ff98d894cfa
mark.j0hnst0n <friendorfaux6@gmail.com>
2020-12-15 11:36:36 +0000

    view recipe function added to app.py and working

--- app.py
+++ app.py
@@ -236,6 +236,12 @@ def search():
     return render_template('search.html', user=session["user"])
 
 
+@app.route('/view_recipe/<recipe_id>')
+def view_recipe(recipe_id):
+    recipe = recipe_collection.find_one({"_id": ObjectId(recipe_id)})
+    return render_template('view_recipe.html', recipe=recipe, user=session["user"])
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
