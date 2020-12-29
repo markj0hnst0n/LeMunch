9ac775dc359321442827d76a213c795a8c2c13ad
mark.j0hnst0n <friendorfaux6@gmail.com>
2020-12-29 14:45:53 +0000

    jquery function by Tim Nelson from task manager mini project added to validate recipe types.  comments added to static js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,50 @@
 $(document).ready(function () {
+    /*
+    jQuery code to initalize some of the js funtionality in Materialize framework
+    */
     $("select").formSelect();
     $(".sidenav").sidenav();
     $(".button-collapse").sidenav();
     $('.tooltipped').tooltip();
     $('.modal').modal();
     $('.fixed-action-btn').floatingActionButton();
+
+    /*
+    Tim Nelson's jQuery function that validates recipe type selections and makes sure the user chooses a recipe type
+    */
+
+    validateMaterializeSelect();
+    function validateMaterializeSelect() {
+        let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+        let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+        if ($("select.validate").prop("required")) {
+            $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+        }
+        $(".select-wrapper input.select-dropdown").on("focusin", function () {
+            $(this).parent(".select-wrapper").on("change", function () {
+                if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                    $(this).children("input").css(classValid);
+                }
+            });
+        }).on("click", function () {
+            if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+                $(this).parent(".select-wrapper").children("input").css(classValid);
+            } else {
+                $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                    if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                        if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                            $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                        }
+                    }
+                });
+            }
+        });
+    }
     
+    /*
+    jQuery functions from Sanwebe.com which create and remove ingredient and method steps boxes dynamically on the screen
+    */
+
     const max_fields      = 10; //maximum input boxes allowed
     const ingredient_wrapper   		= $(".ingredient_fields_wrapper"); // Ingredients fields wrapper
     const method_fields_wrapper   		= $(".method_fields_wrapper"); // Method fields wrapper
